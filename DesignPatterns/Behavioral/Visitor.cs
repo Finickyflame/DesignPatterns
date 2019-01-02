58eaeaa0fc0bdccc506c021e952b4c99f56c2024
Nicholas Berube <berube.nicholas@gmail.com>
2019-01-02 17:23:34 -0500

    Added the Visitor pattern. Removed the check-list in the README.

--- DesignPatterns/Behavioral/Visitor.cs
+++ DesignPatterns/Behavioral/Visitor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Xunit;
 
 namespace DesignPatterns.Behavioral
 {
@@ -6,7 +8,278 @@ namespace DesignPatterns.Behavioral
     {
         public override void Execute()
         {
-            throw new NotImplementedException();
+            var apartment = new Residence
+            {
+                new Bathroom(),
+                new Bedroom(),
+                new Kitchen()
+            };
+
+            var apartmentMaid = new Maid();
+            Assert.Equal(0, apartmentMaid.ServiceCost);
+
+            apartment.Accept(apartmentMaid);
+            Assert.Equal(Maid.BathroomPrice + Maid.BedroomPrice, apartmentMaid.ServiceCost);
+
+            var apartmentPlumber = new Plumber();
+            Assert.Equal(Plumber.BasePrice, apartmentPlumber.ServiceCost);
+
+            apartment.Accept(apartmentPlumber);
+            Assert.Equal(Plumber.BasePrice + Plumber.BathroomPrice + Plumber.KitchenPrice, apartmentPlumber.ServiceCost);
+
+
+            var house = new Residence
+            {
+                new Bathroom(),
+                new Bathroom(),
+
+                new Bedroom(),
+                new Bedroom(),
+                new Bedroom(),
+
+                new Kitchen(),
+                new LivingRoom()
+            };
+
+            var houseMaid = new Maid();
+
+            house.Accept(houseMaid);
+            Assert.Equal(Maid.BathroomPrice * 2 + Maid.BedroomPrice * 3, houseMaid.ServiceCost);
+
+            var housePlumber = new Plumber();
+
+            house.Accept(housePlumber);
+            Assert.Equal(Plumber.BasePrice + Plumber.BathroomPrice * 2 + Plumber.KitchenPrice, housePlumber.ServiceCost);
         }
+
+        #region Definition
+
+        /// <summary>
+        /// Element
+        /// </summary>
+        /// <remarks>
+        /// Defines an Accept operation that takes a visitor as an argument.
+        /// </remarks>
+        public interface IRoom
+        {
+            void Accept(IResidenceVisitor visitor);
+        }
+
+
+        /// <summary>
+        /// Visitor
+        /// </summary>
+        /// <remarks>
+        /// Declares a Visit operation for each Concrete Element in the object structure.
+        /// The operation's name and signature identifies the class that sends the Visit request to the visitor.
+        /// That lets the visitor determine the concrete class of the element being visited, then the visitor can access the elements directly through its particular interface
+        /// </remarks>
+        public interface IResidenceVisitor
+        {
+            void Visit(Bathroom room);
+
+            void Visit(Bedroom room);
+
+            void Visit(Kitchen room);
+
+            void Visit(LivingRoom room);
+        }
+
+        /// <summary>
+        /// Object Structure
+        /// </summary>
+        /// <remarks>
+        /// - Can enumerate its elements.
+        /// - May provide a high-level interface to allow the visitor to visit its elements.
+        /// - May either be a Composite(pattern) or a collection such as a list or a set.
+        /// </remarks>
+        public class Residence : IEnumerable<IRoom>
+        {
+            private readonly IList<IRoom> _rooms;
+
+            public Residence()
+            {
+                this._rooms = new List<IRoom>();
+            }
+
+            public void Accept(IResidenceVisitor visitor)
+            {
+                foreach (IRoom room in this)
+                {
+                    room.Accept(visitor);
+                }
+            }
+
+            public void Add(IRoom room)
+            {
+                this._rooms.Add(room);
+            }
+
+            public IEnumerator<IRoom> GetEnumerator()
+            {
+                return this._rooms.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((IEnumerable)this._rooms).GetEnumerator();
+            }
+        }
+
+        #endregion
+
+        #region Concrete Elements
+
+        /// <summary>
+        /// Concrete Element
+        /// </summary>
+        /// <remarks>
+        /// Implements an Accept operation that takes a visitor as an argument.
+        /// </remarks>
+        public class Bathroom : IRoom
+        {
+            public void Accept(IResidenceVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+
+        /// <summary>
+        /// Concrete Element
+        /// </summary>
+        /// <remarks>
+        /// Implements an Accept operation that takes a visitor as an argument.
+        /// </remarks>
+        public class Bedroom : IRoom
+        {
+            public void Accept(IResidenceVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+
+        /// <summary>
+        /// Concrete Element
+        /// </summary>
+        /// <remarks>
+        /// Implements an Accept operation that takes a visitor as an argument.
+        /// </remarks>
+        public class Kitchen : IRoom
+        {
+            public void Accept(IResidenceVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+
+        /// <summary>
+        /// Concrete Element
+        /// </summary>
+        /// <remarks>
+        /// Implements an Accept operation that takes a visitor as an argument.
+        /// </remarks>
+        public class LivingRoom : IRoom
+        {
+            public void Accept(IResidenceVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+
+        #endregion
+
+        #region Concrete Visitors
+
+        /// <summary>
+        /// Concrete Visitor
+        /// </summary>
+        /// <remarks>
+        /// Implements each operation declared by the Visitor.
+        /// Each operation implements a fragment of the algorithm defined for the corresponding class or object in the structure.
+        /// Concrete Visitor provides the context for the algorithm and stores its local state.
+        /// </remarks>
+        public class Plumber : IResidenceVisitor
+        {
+            public const decimal BasePrice = 100;
+            public const decimal BathroomPrice = 12.7m;
+            public const decimal KitchenPrice = 15.3m;
+
+            public Plumber()
+            {
+                this.ServiceCost = BasePrice;
+            }
+
+            /// <summary>
+            /// State
+            /// </summary>
+            /// <remarks>
+            /// Accumulates results during the traversal of the structure.
+            /// </remarks>
+            public decimal ServiceCost { get; private set; }
+
+            public void Visit(Bathroom room)
+            {
+                this.ServiceCost += BathroomPrice;
+            }
+
+            public void Visit(Bedroom room)
+            {
+                // Does nothing here
+            }
+
+            public void Visit(Kitchen room)
+            {
+                this.ServiceCost += KitchenPrice;
+            }
+
+            public void Visit(LivingRoom room)
+            {
+                // Does nothing here
+            }
+        }
+
+        /// <summary>
+        /// Concrete Visitor
+        /// </summary>
+        /// <remarks>
+        /// Implements each operation declared by the Visitor.
+        /// Each operation implements a fragment of the algorithm defined for the corresponding class or object in the structure.
+        /// Concrete Visitor provides the context for the algorithm and stores its local state.
+        /// </remarks>
+        public class Maid : IResidenceVisitor
+        {
+            public const decimal BathroomPrice = 62.5m;
+            public const decimal BedroomPrice = 54.8m;
+
+            /// <summary>
+            /// State
+            /// </summary>
+            /// <remarks>
+            /// Accumulates results during the traversal of the structure.
+            /// </remarks>
+            public decimal ServiceCost { get; private set; }
+
+            public void Visit(Bathroom room)
+            {
+                this.ServiceCost += BathroomPrice;
+            }
+
+            public void Visit(Bedroom room)
+            {
+                this.ServiceCost += BedroomPrice;
+            }
+
+            public void Visit(Kitchen room)
+            {
+                // Does nothing here
+            }
+
+            public void Visit(LivingRoom room)
+            {
+                // Does nothing here
+            }
+        }
+
+        #endregion
     }
 }
--- README.md
+++ README.md
@@ -7,37 +7,37 @@ Simple repository containing one simple example for all existing patterns in C#.
 
 Patterns that flexibly create and instantiate objects for you.
 
-- [x] [Abstract factory](DesignPatterns/Creational/AbstractFactory.cs) groups object factories that have a common theme.
-- [x] [Builder](DesignPatterns/Creational/Builder.cs) constructs complex objects by separating construction and representation.
-- [x] [Factory method](DesignPatterns/Creational/FactoryMethod.cs) creates objects without specifying the exact class to create.
-- [x] [Prototype](DesignPatterns/Creational/Prototype.cs) creates objects by cloning an existing object.
-- [x] [Singleton](DesignPatterns/Creational/Singleton.cs) restricts object creation for a class to only one instance.
+- [Abstract factory](DesignPatterns/Creational/AbstractFactory.cs) groups object factories that have a common theme.
+- [Builder](DesignPatterns/Creational/Builder.cs) constructs complex objects by separating construction and representation.
+- [Factory method](DesignPatterns/Creational/FactoryMethod.cs) creates objects without specifying the exact class to create.
+- [Prototype](DesignPatterns/Creational/Prototype.cs) creates objects by cloning an existing object.
+- [Singleton](DesignPatterns/Creational/Singleton.cs) restricts object creation for a class to only one instance.
 
 ## Structural
 
 Patterns that define ways to compose objects to obtain new functionality.
  
-- [x] [Adapter](DesignPatterns/Structural/Adapter.cs) allows classes with incompatible interfaces to work together.
-- [x] [Bridge](DesignPatterns/Structural/Bridge.cs) decouples an abstraction from its implementation so that the two can vary independently.
-- [x] [Composite](DesignPatterns/Structural/Composite.cs) composes zero-or-more similar objects so that they can be manipulated as one object.
-- [x] [Decorator](DesignPatterns/Structural/Decorator.cs) dynamically adds/overrides behaviour in an existing method of an object.
-- [x] [Facade](DesignPatterns/Structural/Facade.cs) provides a simplified interface to a large body of code.
-- [x] [Flyweight](DesignPatterns/Structural/Flyweight.cs) reduces the cost of creating and manipulating a large number of similar objects.
-- [x] [Private Class Data](DesignPatterns/Structural/PrivateClassData.cs) restricts exposure of accessor/mutator by limiting their visibility.
-- [x] [Proxy](DesignPatterns/Structural/Proxy.cs) provides a placeholder for another object to control access, reduce cost, and reduce complexity.
+- [Adapter](DesignPatterns/Structural/Adapter.cs) allows classes with incompatible interfaces to work together.
+- [Bridge](DesignPatterns/Structural/Bridge.cs) decouples an abstraction from its implementation so that the two can vary independently.
+- [Composite](DesignPatterns/Structural/Composite.cs) composes zero-or-more similar objects so that they can be manipulated as one object.
+- [Decorator](DesignPatterns/Structural/Decorator.cs) dynamically adds/overrides behaviour in an existing method of an object.
+- [Facade](DesignPatterns/Structural/Facade.cs) provides a simplified interface to a large body of code.
+- [Flyweight](DesignPatterns/Structural/Flyweight.cs) reduces the cost of creating and manipulating a large number of similar objects.
+- [Private Class Data](DesignPatterns/Structural/PrivateClassData.cs) restricts exposure of accessor/mutator by limiting their visibility.
+- [Proxy](DesignPatterns/Structural/Proxy.cs) provides a placeholder for another object to control access, reduce cost, and reduce complexity.
 
 ## Behavioral
 
 Patterns that specifically concern the communication between objects.
 
-- [x]  [Chain of responsibility](DesignPatterns/Behavioral/ChainOfResponsibility.cs) delegates commands to a chain of processing objects.
-- [x]  [Command](DesignPatterns/Behavioral/Command.cs) creates objects which encapsulate actions and parameters.
-- [x]  [Interpreter](DesignPatterns/Behavioral/Interpreter.cs) implements a specialized language.
-- [x]  [Iterator](DesignPatterns/Behavioral/Iterator.cs) accesses the elements of an object sequentially without exposing its underlying representation.
-- [x]  [Mediator](DesignPatterns/Behavioral/Mediator.cs) allows loose coupling between classes by being the only class that has detailed knowledge of their methods.
-- [x]  [Memento](DesignPatterns/Behavioral/Memento.cs) provides the ability to restore an object to its previous state (undo).
-- [x]  [Observer](DesignPatterns/Behavioral/Observer.cs) is a publish/subscribe pattern which allows a number of observer objects to see an event.
-- [x]  [State](DesignPatterns/Behavioral/State.cs) allows an object to alter its behavior when its internal state changes.
-- [x]  [Strategy](DesignPatterns/Behavioral/Strategy.cs) allows one of a family of algorithms to be selected on-the-fly at runtime.
-- [x]  [Template method](DesignPatterns/Behavioral/TemplateMethod.cs) defines the skeleton of an algorithm as an abstract class, allowing its subclasses to provide concrete behavior.
-- [ ]  [Visitor](DesignPatterns/Behavioral/Visitor.cs) separates an algorithm from an object structure by moving the hierarchy of methods into one object.
+- [Chain of responsibility](DesignPatterns/Behavioral/ChainOfResponsibility.cs) delegates commands to a chain of processing objects.
+- [Command](DesignPatterns/Behavioral/Command.cs) creates objects which encapsulate actions and parameters.
+- [Interpreter](DesignPatterns/Behavioral/Interpreter.cs) implements a specialized language.
+- [Iterator](DesignPatterns/Behavioral/Iterator.cs) accesses the elements of an object sequentially without exposing its underlying representation.
+- [Mediator](DesignPatterns/Behavioral/Mediator.cs) allows loose coupling between classes by being the only class that has detailed knowledge of their methods.
+- [Memento](DesignPatterns/Behavioral/Memento.cs) provides the ability to restore an object to its previous state (undo).
+- [Observer](DesignPatterns/Behavioral/Observer.cs) is a publish/subscribe pattern which allows a number of observer objects to see an event.
+- [State](DesignPatterns/Behavioral/State.cs) allows an object to alter its behavior when its internal state changes.
+- [Strategy](DesignPatterns/Behavioral/Strategy.cs) allows one of a family of algorithms to be selected on-the-fly at runtime.
+- [Template method](DesignPatterns/Behavioral/TemplateMethod.cs) defines the skeleton of an algorithm as an abstract class, allowing its subclasses to provide concrete behavior.
+- [Visitor](DesignPatterns/Behavioral/Visitor.cs) separates an algorithm from an object structure by moving the hierarchy of methods into one object.
